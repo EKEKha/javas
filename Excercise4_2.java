746c085fc91fde24bece4d875770ee2fe81ce21e
EKEKha <dmsrud2682@hanmail.net>
2022-06-25 22:46:21 +0900

    modify

--- Excercise4_2.java
+++ Excercise4_2.java
@@ -36,7 +36,43 @@ public class Excercise4_2 {
 	/**[4-7]Math.random()을 이용해서 1부터 6사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하라.**/
 	int value= (int)(Math.random()*6)+1;
 		System.out.println("value="+value);
-	
-	} 
-
+		
+		
+	/**[4-8]방정식 2x+4y=10의 모든 해를 구하시오. 단 x, y는 정수 범위는 0<=x,y<=10*/
+		for(int x=0;x<=10;x++) {
+			for(int y=0;y<=10;y++) {
+				if(2*x+4*y==10) {
+					System.out.println("x="+x+", y="+y);
+				}
+			}
+			
+			
+			
+	/**[4-9]숫자로 이루어진 문자열 'str'이 있을 때 ,각자리의 합을 더한 결과를 출력하는 코드를 완성하라.
+	 * 만일 문자열이 12345 라면 1+2+3+4+5인 결과를 출력해야한다.
+	 */
+			String str ="12345";
+			int sum = 0;
+			
+			for( i=0; i<str.length();i++){
+				sum+=str.charAt(i)-'0'; //문자를 숫자로 변환
+			}
+			System.out.println("sum="+sum);
+			
+			
+	/***[4-10]int 타입의 변수 num이 있을때, 각자리의 합을 더한 결과를 출력하는 코드를 완성하라.
+	 * 만일 변수 num의 값이 12345라면 '1+2+3+4+5'의 결과인 15를 출력하라.****/
+			int num=0;
+			 sum=0;
+			 
+			 while(num>0) {
+				 sum += num%10;//5  4  3 2 1
+				 num/=10;//1234  123 12 1  0
+			 }
+			
+			
+			
+			
+	} //main의 끝
+	}
 }
