d5a41fb503527f94d82ad9578fda37f3540d5d51
EKEKha <dmsrud2682@hanmail.net>
2022-07-01 23:44:11 +0900

    0701

--- Excercise5.java
+++ Excercise5.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Excercise5 {
 
@@ -116,11 +117,72 @@ public class Excercise5 {
 	     for(int i=0; i<6;i++)
 	    	 System.out.printf("ball[%d]=%d%n",i,ball[i]);
 	     
+	     //arr2배열에 code배열값 무작위로 담기
+	     int[]code= {-4,-1,3,6,11}; //불연속적인 값들로 구성된 배열
+	     int[]arr2= new int[10]; 
+	     
+	     for(int i=0;i<arr2.length;i++) {
+	    	 int tmp2=(int)(Math.random()*code.length);// tmp는 0,1,2,3,4,
+	    	 arr2 [i] = code[tmp2];//code 배열 내용이무작위로 중복되서 10개만큼 담겨짐 
+	     }
+	     System.out.println("arr2="+Arrays.toString(arr2));
+	     
+	     
+	     /************길이가 10인 배열에 0~9사이 임의의값 채우고 크기순으로 정렬하는 예제***********************************/
+	     int[] numArr2=new int [10];
+	     
+	     for(int i=0;i<numArr2.length;i++) {
+	    	 System.out.print(numArr2[i]=(int)(Math.random()*10));//길이가 10인 배열에 0~9 임의의 값으로 채운다
+	    	 //numArr2[i]=(int)(Math.random()*10);
+	    	 // System.out.print(numArr2[i]);  두문장을 합친것
+	     }
+	     System.out.println();//ㅈㄹ바꿈
+	     System.out.println("↑초기상태");
+	     
+	     
+    	 /***본격적인 정렬과정*********/
+	     for(int i=0;i<numArr2.length-1;i++) {
+	    	 boolean changed =false;  //자리바꿈이 발생했는지 체크.
+	    	
+	    	 for( j=0;j<numArr2.length-1-i;j++) {//길이가 5이면 4번만 비교 6이면 5번만 비교 그래서 -1
+	    		 //-i을 하는이유 : i=0 즉 첫번째 비교하면 비교작업은 끝까지 다되지만 작업수행후 마지막자리에 최대값이 놓여지게된다
+	    		 //i=1 두번째비교하면 i=0이었을때 마지막자리에 최대값이 정해지게된 상태 굳이 더이상 비교해볼 필요가없어짐
+	    		 if(numArr2[j]>numArr2[j+1]) {
+	    			 int temp3=numArr2[j];
+	    			 numArr2[j]=numArr2[j+1];
+	    			 numArr2[j+1]=temp3;
+	    			 changed=true; // 자리바꿈이 발생했으니 changed를 true로
+	    			  }
+	    		 
+	    	 }//for j 끝
+	    	 if (!changed) break; //자리바꿈이 없으면 반복문 벗어남 changed는 그대로 false고 !changed =true 
+	    	 
+	    	 /**********for문을 이용한 배열출력, 정렬과정을 보기위해 for문 안에 넣었다.*************/
+	    	 for(int k=0;k<numArr2.length;k++) 
+	    		 System.out.print(numArr2[k]); //정렬된 결과를출력
+	    	 System.out.println();	 
+	    	
+	     }//end for i
 	     
 	     
 	     
+	     /**********배열에 담긴값 갯수 알기******************************************/
+	     int [] numArr3=new int[10];
+	     int [] counter=new int[10];//해당 인덱스의 값이 몇번 나타났는 지 알수 있음 numArr[0]의 값이 4 [1]의 값이 4라고 치면
+	     //counter [4]에 1씩 증가하면서 2가됨. numArr3배열에 4개 몇개있는지 알수있음 
+	     
+	     for(int i=0;i<numArr3.length;i++) {
+	    	 numArr3[i]=(int)(Math.random()*10);
+	    	 System.out.print(numArr3[i]);
+	     }System.out.println();
 	     
 	     
+	     for(int i=0;i<numArr3.length;i++) {
+	    	 counter[numArr3[i]]++; //0인 counter 배열에다가 +1씩
+	     }
+	     for(int i=0;i<numArr3.length;i++) {
+	    	 System.out.println(i+"의 개수 :"+counter[i]);
+	     }
 	     
 	}
 
