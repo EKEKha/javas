b6912b33db842bc4eaa2b681b7a8b10a710bf358
EKEKha <dmsrud2682@hanmail.net>
2022-07-13 19:52:09 +0900

    인스턴스메서드

---
+++ chapter6/Excercise6_4.java
@@ -0,0 +1,27 @@
+package chapter6;
+
+class MyPoint{
+	 int x;//인스턴스변수
+	 int y;//인스터스 변수
+
+	 MyPoint(int x,int y){
+		 this.x=x;
+		 this.y=y;
+	 }
+	 
+	 double getDistance(int x1,int y1) {
+			return Math.sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1)); //x,y는 인스턴스 분수
+	 }
+}
+
+public class Excercise6_4 {
+
+	public static void main(String[] args) {
+		MyPoint p = new MyPoint(1,1);
+		
+		//p와 (2,2)의 거리를 구한다.
+		System.out.println(p.getDistance(2, 2));
+	
+	}
+
+}
